f5eb719b29f307250a23fb58817a07c827155261
soon <sudosoon@gmail.com>
2022-06-30 14:01:16 +0900

    쉘 스크립트 수정

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-pm2 start --name lunch lunch.js --watch
-pm2 start --name stock stock.js --watch
+pm2 start --name lunch ~/workspace/soon-slack-bot/lunch.js --watch
+pm2 start --name stock ~/workspace/soon-slack-bot/stock.js --watch
 pm2 monit
